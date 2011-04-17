b6ef420efa597f66d679072751ad70428c66f49c
Marenz <marenz@supradigital.org>
2011-04-17 13:25:06 +0200

    updated testparser to work with the new api. Debugging time now

--- src/tests/parser/testParser.c
+++ src/tests/parser/testParser.c
@@ -1,11 +1,11 @@
-#include <parser.h>
+#include <psyc/parser.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
 
 int main(int argc, char** argv)
 {
-	const uint8_t buffer[2048];
+	uint8_t buffer[2048];
 	int index;
 
 	int file = open(argv[1],O_RDONLY);
@@ -13,61 +13,40 @@ int main(int argc, char** argv)
 		return -1;	
 	index = read(file,(void*)buffer,sizeof(buffer));
 
+
+	PSYC_State state;
+	PSYC_initState(&state);
+
 	unsigned int cursor=0,tmp=0;
-	while(cursor < index) 
+	unsigned long expectedBytes=0;
+	uint8_t modifier;
+	int ret;
+	PSYC_Array name, value;
+
+	PSYC_nextBuffer(&state, PSYC_CreateArray(buffer, index));
+
+	// try parsing that now	
+	while(ret=PSYC_parse(&state, &name, &value, &modifier, &expectedBytes))
 	{
-		int ret;
-		const uint8_t *varname,*varvalue;
-		unsigned int nl,vl;
-		// try parsing that now	
-		while((ret=PSYC_parseHeader(
-					&cursor,
-					buffer,index,
-					&varname,&nl,
-					&varvalue,&vl)) == 0)
+		switch (ret)
 		{
-			write(0,varname,nl);
-			write(0," = ",3);
-			write(0,varvalue,vl);
-			write(0,"\n",1);
-		}
-		printf("ret0: %d\ncursor0: %d\n",
-				ret,cursor);
-
-		if(ret == 3 || ret == 2)
-		{
-			write(0,varname,nl);
-			write(0," = ",3);
-			write(0,varvalue,vl);
-			write(0,"\n",1);
-		}
-		if(ret ==2) // header finished
-		while((ret=PSYC_parseOpenBody(
-						&cursor,
-						buffer,index,
-						&varname,&nl,
-						&varvalue,&vl)) == 0)
-		{
-			write(0,varname,nl);
-			write(0," = ",3);
-			write(0,varvalue,vl);
-			write(0,"\n",1);
+			case PSYC_ROUTING:
+			case PSYC_ENTITY:
+			case PSYC_METHOD:
+				write(0,&modifier,1);
+				write(0,name.ptr, name.length);
+				write(0," = ", 3);
+				write(0,value.ptr, value.length);
+				write(0,"\n", 1);
+				break;
+			case PSYC_COMPLETE:
+				write(0, "Done parsing.\n", 15);
+				continue;
+			default:
+				printf("Error while parsing: %i\n", ret);
+				return;
 		}
 
-		if(ret == 3 || ret == 2)
-		{
-			write(0,varname,nl);
-			write(0," = ",3);
-			write(0,varvalue,vl);
-			write(0,"\n",1);
-		}
-
-		if(tmp != cursor)
-			tmp=cursor;
-		else
-			return 1;
-		printf("ret: %d\ncursor: %d\n--------\n",
-				ret,cursor);
 	}
 	return 0;
 
