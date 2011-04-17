2294dabea2da80ad3573e4af0e0615f6909b047b
Marenz <marenz@supradigital.org>
2011-04-17 14:47:25 +0200

    added some documentation

--- include/psyc/parser.h
+++ include/psyc/parser.h
@@ -36,13 +36,22 @@ typedef struct
 } PSYC_State;
 
 #ifndef PSYC_COMPILE_LIBRARY
+/* @brief shortcut for creating an array 
+ *
+ * @param memory pointer to the buffer
+ * @param length length of that buffer
+ *
+ * @returns an instance of the PSYC_Array struct */
 inline PSYC_Array PSYC_CreateArray (uint8_t* const memory, unsigned int length)
 {
 	PSYC_Array arr = {length, memory};
 
 	return arr;
 }
-
+/* @brief initiates the state struct with flags 
+ *
+ * @param state pointer to the state struct that should be initiated
+ * @param flags the flags that one ones to set, se PSYC_Flags */
 inline void PSYC_initState2 (PSYC_State* state, uint8_t flags )
 {
 	memset(state, 0, sizeof(PSYC_State));
