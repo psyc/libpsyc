41c1c5c9f8cc3696b51eed20f0d9a26f27d73c8e
tg(x) <*@tg-x.net>
2011-04-30 15:46:17 +0200

    +

--- src/text.c
+++ src/text.c
@@ -1,2 +1,9 @@
 /* psyc_text() */
 
+int psyc_text(char *template, size_t  tlen,
+              char **buffer, size_t *blen,
+              psyctextCB lookupValue,
+              char *braceOpen, char *braceClose)
+{
+
+}
