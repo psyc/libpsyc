1d3de48dea97cb917583ca4a0c35b02eab78835a
Marenz <marenz@supradigital.org>
2011-05-09 00:15:03 +0200

    synchronized the D binding

--- include/d/psyc/parser.d
+++ include/d/psyc/parser.d
@@ -21,8 +21,8 @@ extern (C):
 
 enum ParseFlag
 {
-	PARSE_HEADER_ONLY = 1,
-	PARSE_BEGIN_AT_CONTENT = 2,
+	ROUTING_ONLY = 1,
+	START_AT_CONTENT = 2,
 }
 
 /**
@@ -31,43 +31,66 @@ enum ParseFlag
  */
 enum ParseRC
 {
-	PARSE_ERROR_END = -9,
-	PARSE_ERROR_BODY = -8,
-	PARSE_ERROR_METHOD = -7,
-	PARSE_ERROR_MOD_NL = -6,
-	PARSE_ERROR_MOD_LEN = -5,
-	PARSE_ERROR_MOD_TAB = -4,
-	PARSE_ERROR_MOD_NAME = -3,
-	PARSE_ERROR_LENGTH = -2,
-	PARSE_ERROR = -1,
-	PARSE_SUCCESS = 0,
+/// Error, packet is not ending with a valid delimiter.
+	ERROR_END = -8,
+/// Error, expected NL after the method.
+	ERROR_METHOD = -7,
+/// Error, expected NL after a modifier.
+	ERROR_MOD_NL = -6,
+/// Error, modifier length is not numeric.
+	ERROR_MOD_LEN = -5,
+/// Error, expected TAB before modifier value.
+	ERROR_MOD_TAB = -4,
+/// Error, modifier name is missing.
+	ERROR_MOD_NAME = -3,
+/// Error, expected NL after the content length.
+	ERROR_LENGTH = -2,
+/// Error in packet.
+	ERROR = -1,
+// Success, used internally.
+	SUCCESS = 0,
 /// Buffer contains insufficient amount of data.
 /// Fill another buffer and concatenate it with the end of the current buffer,
 /// from the cursor position to the end.
-	PARSE_INSUFFICIENT = 1,
+	INSUFFICIENT = 1,
 /// Routing modifier parsing done.
 /// Operator, name & value contains the respective parts.
-	PARSE_ROUTING = 2,
-/// Entity modifier parsing done.
-/// Operator, name & value contains the respective parts.
-	PARSE_ENTITY = 3,
-/// Entity modifier parsing is incomplete.
+	ROUTING = 2,
+/// Start of an incomplete entity modifier.
 /// Operator & name are complete, value is incomplete.
-	PARSE_ENTITY_INCOMPLETE = 4,
-/// Body parsing done, name contains method, value contains body.
-	PARSE_BODY = 5,
-/// Body parsing is incomplete, name contains method, value contains part of the body.
-	PARSE_BODY_INCOMPLETE = 6,
-/// Content parsing done, value contains the whole content.
-/// Used when PSYC_PARSE_ROUTING_ONLY is set.
-	PARSE_CONTENT = 5,
-/// Content parsing is incomplete, value contains part of content.
-/// Used when PSYC_PARSE_ROUTING_ONLY is set.
-	PARSE_CONTENT_INCOMPLETE = 6,
-/// Reached end of packet, parsing done.
-	PARSE_COMPLETE = 7,
-/// Binary value parsing incomplete, used internally.
-	PARSE_INCOMPLETE = 8,
+	ENTITY_START = 3,
+/// Continuation of an incomplete entity modifier.
+	ENTITY_CONT = 4,
+/// End of an incomplete entity modifier.
+	ENTITY_END = 5,
+/// Entity modifier parsing done in one go.
+/// Operator, name & value contains the respective parts.
+	ENTITY = 6,
+/// Start of an incomplete body.
+/// Name contains method, value contains part of the body.
+	BODY_START = 7,
+/// Continuation of an incomplete body.
+	BODY_CONT = 8,
+/// End of an incomplete body.
+	BODY_END = 9,
+/// Body parsing done in one go, name contains method, value contains body.
+	BODY = 10,
+/// Start of an incomplete content, value contains part of content.
+/// Used when ROUTING_ONLY is set.
+	CONTENT_START = 7,
+/// Continuation of an incomplete body.
+/// Used when ROUTING_ONLY is set.
+	CONTENT_CONT = 8,
+/// End of an incomplete body.
+/// Used when ROUTING_ONLY is set.
+	CONTENT_END = 9,
+/// Content parsing done in one go, value contains the whole content.
+/// Used when ROUTING_ONLY is set.
+	CONTENT = 10,
+// Binary value parsing complete, used internally.
+	COMPLETE = 11,
+// Binary value parsing incomplete, used internally.
+	INCOMPLETE = 12,
 }
 
 /**
@@ -76,17 +99,17 @@ enum ParseRC
  */
 enum ParseListRC
 {
-	PARSE_LIST_ERROR_DELIM = -5,
-	PARSE_LIST_ERROR_LEN = -4,
-	PARSE_LIST_ERROR_TYPE = -3,
-	PARSE_LIST_ERROR_NAME = -2,
-	PARSE_LIST_ERROR= -1,
+	LIST_ERROR_DELIM = -5,
+	LIST_ERROR_LEN   = -4,
+	LIST_ERROR_TYPE  = -3,
+	LIST_ERROR_NAME  = -2,
+	LIST_ERROR       = -1,
 /// Completed parsing a list element.
-	PARSE_LIST_ELEM = 1,
+	LIST_ELEM        =  1,
 /// Reached end of buffer.
-	PARSE_LIST_END = 2,
+	LIST_END         =  2,
 /// Binary list is incomplete.
-	PARSE_LIST_INCOMPLETE = 3,
+	LIST_INCOMPLETE  =  3,
 }
 
 /**
@@ -106,6 +129,118 @@ struct ParseState
 	Bool contentLengthFound; ///< is there a length given for this packet?
 	size_t valueParsed; ///< number of bytes parsed from the value so far
 	size_t valueLength; ///< expected length of the value
+	Bool valueLengthFound; ///< is there a length given for this modifier?
+
+	/**
+	 * Initiates the state struct.
+	 */
+	static ParseState opCall ( )
+	{
+		ParseState inst;
+		return inst;
+	}
+
+	/**
+	 * Initializes the state struct with flags.
+	 *
+	 * @param flags Flags to be set for the parser, see ParseFlag.
+	 */
+	static ParseState opCall ( ubyte flags )
+	{
+		ParseState inst;
+		inst.flags = flags;
+
+		if (flags & ParseFlag.ROUTING_ONLY)
+			inst.part = Part.CONTENT;
+
+		return inst;
+	}
+
+	/**
+	 * Sets a new buffer in the parser state struct with data to be parsed.
+	 *
+	 * @param buffer the buffer that should be parsed now
+	 * @see String
+	 */
+	void setParseBuffer ( String buffer )
+	{
+		this.buffer = buffer;
+		this.cursor = 0;
+
+		if (this.flags & ParseFlag.START_AT_CONTENT)
+		{
+			this.contentLength      = buffer.length;
+			this.contentLengthFound = Bool.TRUE;
+		}
+	}
+
+	/**
+	 * Sets a new buffer in the parser state struct with data to be parsed.
+	 *
+	 * @param buffer the buffer that should be parsed now
+	 * @see String
+	 */
+	void setParseBuffer ( ubyte[] buffer )
+	{
+		this.setParseBuffer(*(cast(String*) &buffer));
+	}
+
+	/**
+	 * Sets a new buffer in the parser state struct with data to be parsed.
+	 *
+	 * @param buffer pointer to the buffer that should be parsed now
+	 * @param length length of the buffer
+	 */
+	void setParseBuffer ( ubyte* buffer, size_t length )
+	{
+		this.setParseBuffer(String(length, cast(ubyte*)buffer));
+	}
+
+	size_t getContentLength ( )
+	{
+		return this.contentLength;
+	}
+
+	bool isContentLengthFound ( )
+	{
+		return this.contentLengthFound == Bool.TRUE;
+	}
+
+	size_t getValueLength ( )
+	{
+		return this.valueLength;
+	}
+
+	bool isValueLengthFound ( )
+	{
+		return this.valueLengthFound == Bool.TRUE;
+	}
+
+	size_t getCursor ( )
+	{
+		return this.cursor;
+	}
+
+	size_t getBufferLength ( )
+	{
+		return this.buffer.length;
+	}
+
+	size_t getRemainingLength ( )
+	{
+		return this.buffer.length - this.cursor;
+	}
+
+	ubyte* getRemainingBuffer ( )
+	{
+		return this.buffer.ptr + this.cursor;
+	}
+
+	void getRemainingBuffer ( ref ubyte[] buf )
+	{
+		buf = this.buffer.ptr[cursor .. getRemainingLength()];
+	}
+
 }
 
 /**
@@ -120,35 +255,37 @@ struct ParseListState
 
 	size_t elemParsed; ///< number of bytes parsed from the elem so far
 	size_t elemLength; ///< expected length of the elem
+
+	/**
+	 * Sets a new buffer with data to be parsed
+	 * @param buffer the buffer to be parsed
+	 */
+	void setBuffer ( String buffer )
+	{
+		this.buffer = buffer;
+		this.cursor = 0;
+	}
+
+	/**
+	 * Sets a new buffer with data to be parsed
+	 * @param buffer the buffer to be parsed
+	 */
+	void setBuffer ( ubyte[] buffer )
+	{
+		this.setBuffer(*(cast(String*) &buffer));
+	}
+
+	/**
+	 * Sets a new buffer with data to be parsed
+	 * @param buffer pointer to the buffer to be parsed
+	 * @param length size of the buffer
+	 */
+	void setBuffer ( ubyte* buffer, size_t length )
+	{
+		this.setBuffer(String(length, buffer));
+	}
 }
 
-/**
- * Initiates the state struct.
- *
- * @param state Pointer to the state struct that should be initiated.
- */
-void psyc_initParseState (ParseState* state);
-
-/**
- * Initiates the state struct with flags.
- *
- * @param state Pointer to the state struct that should be initiated.
- * @param flags Flags to be set for the parser, see ParseFlag.
- */
-void psyc_initParseState2 (ParseState* state, ubyte flags);
-
-/**
- * Initiates the list state struct.
- *
- * @param state Pointer to the list state struct that should be initiated.
- */
-void psyc_initParseListState (ParseListState* state);
-
-void psyc_nextParseBuffer (ParseState* state, String newBuf);
-
-void psyc_nextParseListBuffer (ParseListState* state, String newBuf);
-
-size_t psyc_getContentLength (ParseState* s);
 
 /**
  * Parse PSYC packets.
