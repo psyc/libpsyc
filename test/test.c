4b9ea95d3bc0aad7e467961ffef81132ed927242
tg(x) <*@tg-x.net>
2011-05-14 20:13:55 +0200

    test: buf size option for files too

--- test/test.c
+++ test/test.c
@@ -34,7 +34,10 @@ void *get_in_addr (struct sockaddr *sa) {
 	return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-void test_file(const char* filename) {
+void test_file(const char* filename, size_t recv_buf_size) {
+	if (recv_buf_size <= 0)
+		recv_buf_size = RECV_BUF_SIZE;
+
 	char buf[CONT_BUF_SIZE + RECV_BUF_SIZE];  // cont buf + recv buf: [  ccrrrr]
 	char *recvbuf = buf + CONT_BUF_SIZE;      // recv buf:                 ^^^^
 	size_t nbytes;
@@ -46,11 +49,15 @@ void test_file(const char* filename) {
 	}
 
 	test_init(0);
-	while ((nbytes = read(fd, (void*)recvbuf, RECV_BUF_SIZE)))
+
+	while ((nbytes = read(fd, (void*)recvbuf, recv_buf_size)))
 		test_input(0, recvbuf, nbytes);
 }
 
 void test_server(const char* port, size_t recv_buf_size) {
+	if (recv_buf_size <= 0)
+		recv_buf_size = RECV_BUF_SIZE;
+
 	char buf[CONT_BUF_SIZE + RECV_BUF_SIZE];  // cont buf + recv buf: [  ccrrrr]
 	char *recvbuf = buf + CONT_BUF_SIZE;      // recv buf:                 ^^^^
 
@@ -71,9 +78,6 @@ void test_server(const char* port, size_t recv_buf_size) {
 
 	struct addrinfo hints, *ai, *p;
 	struct timeval start[NUM_PARSERS], end[NUM_PARSERS];
-
-	if (recv_buf_size <= 0)
-		recv_buf_size = RECV_BUF_SIZE;
 
 	FD_ZERO(&master);    // clear the master and temp sets
 	FD_ZERO(&read_fds);
