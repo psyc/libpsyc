9f3cc511aa6386b9db86b309f7a4fdc4bc73f4a4
Gabor Adam Toth <tg@tgbit.net>
2011-11-03 14:44:04 +0100

    c2str

--- src/variable.c
+++ src/variable.c
@@ -5,42 +5,42 @@
 /// Routing variables in alphabetical order.
 const PsycString psyc_routing_vars[] =
 {
-	PSYC_C2STR("_amount_fragments"),
-	PSYC_C2STR("_context"),
-	//PSYC_C2STR("_count"),			// older PSYC
-	PSYC_C2STR("_counter"),			// the name for this is supposed to be _count, not _counter
-	PSYC_C2STR("_fragment"),
-	//PSYC_C2STR("_length"),		// older PSYC
-	PSYC_C2STR("_source"),
-	//PSYC_C2STR("_source_identification"),	// older PSYC
-	PSYC_C2STR("_source_identity"),
-	PSYC_C2STR("_source_relay"),
-	PSYC_C2STR("_source_relay_relay"),	// until you have a better idea.. is this really in use?
-	PSYC_C2STR("_tag"),
-	PSYC_C2STR("_tag_relay"),
-	//PSYC_C2STR("_tag_reply"),		// older PSYC
-	PSYC_C2STR("_target"),
-	PSYC_C2STR("_target_forward"),
-	PSYC_C2STR("_target_relay"),
-	//PSYC_C2STR("_understand_modules"),	// older PSYC
-	//PSYC_C2STR("_using_modules"),		// older PSYC
+	PSYC_C2STRI("_amount_fragments"),
+	PSYC_C2STRI("_context"),
+	//PSYC_C2STRI("_count"),			// older PSYC
+	PSYC_C2STRI("_counter"),			// the name for this is supposed to be _count, not _counter
+	PSYC_C2STRI("_fragment"),
+	//PSYC_C2STRI("_length"),		// older PSYC
+	PSYC_C2STRI("_source"),
+	//PSYC_C2STRI("_source_identification"),	// older PSYC
+	PSYC_C2STRI("_source_identity"),
+	PSYC_C2STRI("_source_relay"),
+	PSYC_C2STRI("_source_relay_relay"),	// until you have a better idea.. is this really in use?
+	PSYC_C2STRI("_tag"),
+	PSYC_C2STRI("_tag_relay"),
+	//PSYC_C2STRI("_tag_reply"),		// older PSYC
+	PSYC_C2STRI("_target"),
+	PSYC_C2STRI("_target_forward"),
+	PSYC_C2STRI("_target_relay"),
+	//PSYC_C2STRI("_understand_modules"),	// older PSYC
+	//PSYC_C2STRI("_using_modules"),		// older PSYC
 };
 
 // Variable types in alphabetical order.
 const PsycDictInt psyc_var_types[] =
 {
-	{PSYC_C2STR("_amount"),   PSYC_TYPE_AMOUNT},
-	{PSYC_C2STR("_color"),    PSYC_TYPE_COLOR},
-	{PSYC_C2STR("_date"),     PSYC_TYPE_DATE},
-	{PSYC_C2STR("_degree"),   PSYC_TYPE_DEGREE},
-	{PSYC_C2STR("_entity"),   PSYC_TYPE_ENTITY},
-	{PSYC_C2STR("_flag"),     PSYC_TYPE_FLAG},
-	{PSYC_C2STR("_language"), PSYC_TYPE_LANGUAGE},
-	{PSYC_C2STR("_list"),     PSYC_TYPE_LIST},
-	{PSYC_C2STR("_nick"),     PSYC_TYPE_NICK},
-	{PSYC_C2STR("_page"),     PSYC_TYPE_PAGE},
-	{PSYC_C2STR("_uniform"),  PSYC_TYPE_UNIFORM},
-	{PSYC_C2STR("_time"),     PSYC_TYPE_TIME},
+	{PSYC_C2STRI("_amount"),   PSYC_TYPE_AMOUNT},
+	{PSYC_C2STRI("_color"),    PSYC_TYPE_COLOR},
+	{PSYC_C2STRI("_date"),     PSYC_TYPE_DATE},
+	{PSYC_C2STRI("_degree"),   PSYC_TYPE_DEGREE},
+	{PSYC_C2STRI("_entity"),   PSYC_TYPE_ENTITY},
+	{PSYC_C2STRI("_flag"),     PSYC_TYPE_FLAG},
+	{PSYC_C2STRI("_language"), PSYC_TYPE_LANGUAGE},
+	{PSYC_C2STRI("_list"),     PSYC_TYPE_LIST},
+	{PSYC_C2STRI("_nick"),     PSYC_TYPE_NICK},
+	{PSYC_C2STRI("_page"),     PSYC_TYPE_PAGE},
+	{PSYC_C2STRI("_uniform"),  PSYC_TYPE_UNIFORM},
+	{PSYC_C2STRI("_time"),     PSYC_TYPE_TIME},
 };
 
 const size_t psyc_routing_vars_num = PSYC_NUM_ELEM(psyc_routing_vars);
