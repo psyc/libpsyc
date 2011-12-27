07901658f30becb5c0f5661b0da6c695e59247e8
tg(x) <*@tg-x.net>
2011-12-27 17:50:25 +0100

    hello methods

--- src/templates.h
+++ src/templates.h
@@ -4,9 +4,13 @@
      _("You enter [_nick_place]" ),
 ._echo_context_leave =
      _("You leave [_nick_place]"),
+._echo_hello_offer =
+     _("Received hello message"),
 ._notice_context_enter =
      _("[_nick] enters [_nick_place]"),
 ._notice_context_leave =
      _("[_nick] leaves [_nick_place]"),
+._status_hello =
+     _("Your hello is [_hello]"),
 ._request_context_enter =
      _("[_nick] asks for your permission to enter [_nick_place]"),
