3cc1c8127af18ff21a6b532fc0ee2a844422d56d
tg(x) <*@tg-x.net>
2013-07-16 16:53:51 +0200

    license header

--- include/psyc/parse.h
+++ include/psyc/parse.h
@@ -1,3 +1,23 @@
+/*
+  This file is part of libpsyc.
+  Copyright (C) 2011,2012 Carlo v. Loesch, Gabor X Toth, Mathias L. Baumann,
+  and other contributing authors.
+
+  libpsyc is free software: you can redistribute it and/or modify it under the
+  terms of the GNU Affero General Public License as published by the Free
+  Software Foundation, either version 3 of the License, or (at your option) any
+  later version. As a special exception, libpsyc is distributed with additional
+  permissions to link libpsyc libraries with non-AGPL works.
+
+  libpsyc is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+  details.
+
+  You should have received a copy of the GNU Affero General Public License and
+  the linking exception along with libpsyc in a COPYING file.
+*/
+
 #ifndef PSYC_PARSE_H
 #define PSYC_PARSE_H
 
